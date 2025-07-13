06fd3cc7923330286ea0b7045b8a6db89a729f44
Marcelo <marcelopacobello@gmail.com>
2025-07-13 13:38:31 -0300

    feat: adiciona classe principal da aplicação Spring Boot
    - Cria OrangebankBackendApplication com @SpringBootApplication
    - Configura método main para inicializar a aplicação
    - Define pacote base para escaneamento de componentes

---
+++ src/main/java/com/orangejuice/orangebank_backend/OrangebankBackendApplication.java
@@ -0,0 +1,13 @@
+package com.orangejuice.orangebank_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangebankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangebankBackendApplication.class, args);
+	}
+
+}
