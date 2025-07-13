bf494122446c65f027564019b8cc9ead2bfcb47b
Marcelo <marcelopacobello@gmail.com>
2025-07-13 14:11:57 -0300

    feat: adiciona Swagger/OpenAPI para documentação da API

---
+++ src/test/java/com/orangejuice/orangebank_backend/OrangebankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orangejuice.orangebank_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangebankBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
